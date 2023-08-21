e691385b12caa242ef6f4682f02c720c4245db5f
Rod97139 <fayaflame@gmail.com>
2023-08-21 23:22:47 +0200

    create class article

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -1,3 +1,22 @@
 /**
  * Représentation du format d'un article ainsi que le formatage des dates pour affichage
  */
+
+class Article {
+    constructor(jsonArticle) {
+        // this.id = jsonArticle.id;
+        // this.title = jsonArticle.title;
+        // this.content = jsonArticle.content;
+        // this.author = jsonArticle.author;
+        // this.date = new Date(jsonArticle.date);
+        // this.date = this.date.toLocaleDateString('fr-FR', {year: 'numeric', month: 'long', day: 'numeric'});
+        //appliquer en une ligne une copie de tout les propriétés de jsonArticle dans this Article
+        jsonArticle && Object.assign(this, jsonArticle);
+    }
+
+    getFormattedDate(article) {
+        let timestamp = Date.parse(this.publicationDate);
+        let date = new Date(timestamp);
+        return date.toLocaleDateString();
+    }
+}
